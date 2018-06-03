f73952a9906456656fbbf3d9b1bf14dd1a782e6d
Liam Zdenek <liamzdenek@gmail.com>
2018-06-03 10:16:59 -0500

    index.js cleanup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 'use strict'
 
-var request = require('request');
-var fs = require('fs');
-var readline = require('readline');
 const MauticAPI = require('./MauticAPI').default;
-
-module.exports.config = {
-	base_url: "https://example.mautic.com",
-	redirect_uri:"https://example.mautic.com/redirecthere",
-	public_key: "EXAMPLE_PUBLIC_KEY",
-	secret_key: "EXAMPLE_SECRET_KEY",
-	state: "RANDOMSTATE",
-	code: "",
-	api_endpoint: "https://example.mautic.com/api"
-};
-
 module.exports.default = MauticAPI;
